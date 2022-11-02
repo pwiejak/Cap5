7a1f372c48908e0fc70d9bd339c2fb1f69a0c415
Paweł Wiejak <pwiejak@hotmail.com>
2022-11-02 23:23:47 +0100

    player surname and email db columns made nullable

---
+++ CapFive.Data/Migrations/20221102222141_player surname and email nullable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapFive.Data.Migrations
+{
+    public partial class playersurnameandemailnullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- CapFive.Data/Model/Player.cs
+++ CapFive.Data/Model/Player.cs
@@ -9,7 +9,7 @@ namespace CapFive.Data.Model
         [Required]
         public string Name { get; set; }
 
-        public string Surname { get; set; }
-        public string Email { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
     }
 }
